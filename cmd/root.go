4bf109ce7794942fad2586a8c511bda39f621360
tetor <tetor.takashi.etou@gmail.com>
2017-08-09 07:42:13 +0900

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,9 @@ import (
     "fmt"
     "os"
 
-	homedir "github.com/mitchellh/go-homedir"
+	// homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	// "github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -19,50 +19,50 @@ var RootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() error {
+func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		return err
+		fmt.Println(err)
+		os.Exit(-1)
 	}
-    return nil
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.purin.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// // Here you will define your flags and configuration settings.
+	// // Cobra supports persistent flags, which, if defined here,
+	// // will be global for your application.
+	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.purin.yaml)")
+    //
+	// // Cobra also supports local flags, which will only run
+	// // when this action is called directly.
+	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".purin" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".purin")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	// TODO: purin path setting
+	//
+	// if cfgFile != "" {
+	// 	// Use config file from the flag.
+	// 	viper.SetConfigFile(cfgFile)
+	// } else {
+	// 	// Find home directory.
+	// 	home, err := homedir.Dir()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(1)
+	// 	}
+    //
+	// 	// Search config in home directory with name ".purin" (without extension).
+	// 	viper.AddConfigPath(home)
+	// 	viper.SetConfigName(".purin")
+	// }
+    //
+	// viper.AutomaticEnv() // read in environment variables that match
+    //
+	// // If a config file is found, read it in.
+	// if err := viper.ReadInConfig(); err == nil {
+	// 	fmt.Println("Using config file:", viper.ConfigFileUsed())
+	// }
 }
